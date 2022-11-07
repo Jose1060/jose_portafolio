a2fc0cec08d247e5cc7be3f0d3b25a779cc778f7
Jose1060 <josean-1060@hotmail.com>
2022-11-06 23:49:02 -0500

    first commit

---
+++ src/components/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+
+type Props = {};
+
+const Home = (props: Props) => {
+	return <div className="bg-[#0a192f]">Home</div>;
+};
+
+export default Home;
