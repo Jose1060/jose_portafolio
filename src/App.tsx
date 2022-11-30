a0abe51af99808f8040f5dc2a5cdeefcc0f6c1a7
Jose1060 <84330500+Jose1060@users.noreply.github.com>
2022-11-30 22:40:32 +0000

    Objetivos completados, Foda falta

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import "./App.css";
 import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Objectives from "./components/Objetives";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
+import Foda from "./components/Foda";
 
 function App() {
 	return (
@@ -11,8 +13,11 @@ function App() {
 			<Navbar />
 			<Home />
 			<About />
+			<Objectives/>
 			<Skills />
+			<Foda/>
 			<Work />
+			
 		</div>
 	);
 }
---
+++ src/components/Foda.tsx
@@ -0,0 +1,115 @@
+import React from 'react'
+import { BsFillHandThumbsUpFill } from 'react-icons/bs'
+
+type Props = {}
+
+const Foda = (props: Props) => {
+  return (
+    <div className="h-screen bg-[#0a192f] text-gray-300 flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center max-w-[1000px] h-full"> 
+        <div className='grid grid-cols-2 gap-10'>
+            <div className='h-[500px] w-[500px] bg-white rounded-lg flex flex-col p-10'>
+                <div className='flex flex-row justify-between '> <BsFillHandThumbsUpFill size={40} style={{color: "##60a5fa"}}/> <h2 className='text-[35px] text-blue-400 font-semibold'>Fortalezas</h2></div>
+                <div>
+                    <div className='my-4'>
+                        <span className='text-[17px] text-blue-400'>PUNTUALIDAD</span>
+                        <p className='text-[15px] text-gray-500'>Soy una persona puntual en los compromisos con personas, ya sea trabajos, reuniones, tareas, etc.</p>
+                    </div>
+
+                    <div className='my-4'>
+                        <span className='text-[17px] text-blue-400'>DISCIPLINADO</span>
+                        <p className='text-[15px] text-gray-500'>Soy una persona que cumple con los principios que cree para llegar a sus metas.</p>
+                    </div>
+
+                    <div className='my-4'>
+                        <span className='text-[17px] text-blue-400'>RESPONSABLE</span>
+                        <p className='text-[15px] text-gray-500'>Soy una persona que cumple con sus compromisos de forma eficiente.</p>
+                    </div>
+
+                    <div className='my-4'>
+                        <span className='text-[17px] text-blue-400'>AUTODIDACTA</span>
+                        <p className='text-[15px] text-gray-500'>Soy una persona que tiende a aprender más por su cuenta o enseñando a los demás.</p>
+                    </div>
+                    </div>
+                
+            </div>
+            <div className='h-[500px] w-[500px] bg-white rounded-lg flex flex-col p-10'>
+                <div className='flex flex-row-reverse justify-between '> <BsFillHandThumbsUpFill size={40} style={{color: "##60a5fa"}}/> <h2 className='text-[35px] text-orange-400 font-semibold'>Debilidades</h2></div>
+                <div>
+                    <div className='my-4'>
+                        <span className='text-[17px] text-blue-400'>PUNTUALIDAD</span>
+                        <p className='text-[15px] text-gray-500'>Soy una persona puntual en los compromisos con personas, ya sea trabajos, reuniones, tareas, etc.</p>
+                    </div>
+
+                    <div className='my-4'>
+                        <span className='text-[17px] text-blue-400'>DISCIPLINADO</span>
+                        <p className='text-[15px] text-gray-500'>Soy una persona que cumple con los principios que cree para llegar a sus metas.</p>
+                    </div>
+
+                    <div className='my-4'>
+                        <span className='text-[17px] text-blue-400'>RESPONSABLE</span>
+                        <p className='text-[15px] text-gray-500'>Soy una persona que cumple con sus compromisos de forma eficiente.</p>
+                    </div>
+
+                    <div className='my-4'>
+                        <span className='text-[17px] text-blue-400'>AUTODIDACTA</span>
+                        <p className='text-[15px] text-gray-500'>Soy una persona que tiende a aprender más por su cuenta o enseñando a los demás.</p>
+                    </div>
+                    </div>
+                
+            </div><div className='h-[500px] w-[500px] bg-white rounded-lg flex flex-col p-10'>
+                <div className='flex flex-row justify-between '> <BsFillHandThumbsUpFill size={40} style={{color: "##60a5fa"}}/> <h2 className='text-[35px] text-green-400 font-semibold'>Oportunidades</h2></div>
+                <div>
+                    <div className='my-4'>
+                        <span className='text-[17px] text-blue-400'>PUNTUALIDAD</span>
+                        <p className='text-[15px] text-gray-500'>Soy una persona puntual en los compromisos con personas, ya sea trabajos, reuniones, tareas, etc.</p>
+                    </div>
+
+                    <div className='my-4'>
+                        <span className='text-[17px] text-blue-400'>DISCIPLINADO</span>
+                        <p className='text-[15px] text-gray-500'>Soy una persona que cumple con los principios que cree para llegar a sus metas.</p>
+                    </div>
+
+                    <div className='my-4'>
+                        <span className='text-[17px] text-blue-400'>RESPONSABLE</span>
+                        <p className='text-[15px] text-gray-500'>Soy una persona que cumple con sus compromisos de forma eficiente.</p>
+                    </div>
+
+                    <div className='my-4'>
+                        <span className='text-[17px] text-blue-400'>AUTODIDACTA</span>
+                        <p className='text-[15px] text-gray-500'>Soy una persona que tiende a aprender más por su cuenta o enseñando a los demás.</p>
+                    </div>
+                    </div>
+                
+            </div><div className='h-[500px] w-[500px] bg-white rounded-lg flex flex-col p-10'>
+                <div className='flex flex-row-reverse justify-between '> <BsFillHandThumbsUpFill size={40} style={{color: "##60a5fa"}}/> <h2 className='text-[35px] text-red-400 font-semibold'>Amenazas</h2></div>
+                <div>
+                    <div className='my-4'>
+                        <span className='text-[17px] text-blue-400'>PUNTUALIDAD</span>
+                        <p className='text-[15px] text-gray-500'>Soy una persona puntual en los compromisos con personas, ya sea trabajos, reuniones, tareas, etc.</p>
+                    </div>
+
+                    <div className='my-4'>
+                        <span className='text-[17px] text-blue-400'>DISCIPLINADO</span>
+                        <p className='text-[15px] text-gray-500'>Soy una persona que cumple con los principios que cree para llegar a sus metas.</p>
+                    </div>
+
+                    <div className='my-4'>
+                        <span className='text-[17px] text-blue-400'>RESPONSABLE</span>
+                        <p className='text-[15px] text-gray-500'>Soy una persona que cumple con sus compromisos de forma eficiente.</p>
+                    </div>
+
+                    <div className='my-4'>
+                        <span className='text-[17px] text-blue-400'>AUTODIDACTA</span>
+                        <p className='text-[15px] text-gray-500'>Soy una persona que tiende a aprender más por su cuenta o enseñando a los demás.</p>
+                    </div>
+                    </div>
+                
+            </div>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Foda
